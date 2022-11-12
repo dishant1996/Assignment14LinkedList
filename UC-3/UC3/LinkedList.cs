d936bd7575306286f032a7053520b3716ba50113
Dishant Dilip Yashwante <113447476+dishant1996@users.noreply.github.com>
2022-11-12 21:58:20 +0530

    Implementing LinkedList UC14 Assignment

---
+++ UC-3/UC3/LinkedList.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace LinkedListProgram
+{
+    internal class LinkedList
+    {
+        internal Node head;
+        internal void Add(int data)
+        {
+            Node Node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = Node;
+            }
+            else
+            {
+                Node temp = this.head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = Node;
+            }
+            Console.WriteLine("{0} is inserted into LinkedList", Node.data);
+        }
+        internal void Append(int data)
+        {
+            Add(data);
+        }
+        public void Display()
+        {
+            Console.WriteLine("Displaying Nodes:");
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("LinkedList is Empty");
+                return;
+            }
+            else
+            {
+                while (temp != null)
+                {
+                    Console.Write(" " + temp.data + " ");
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+}
---
+++ UC-3/UC3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkedListProgram
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the LinkedList Program");
+
+            LinkedList linkedList = new LinkedList();
+
+
+            Console.WriteLine("Adding  elements into Linkedlist using AddLast Method:");
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);
+            linkedList.Append(25);
+            linkedList.Display();
+
+            Console.ReadLine();
+
+        }
+    }
+}
